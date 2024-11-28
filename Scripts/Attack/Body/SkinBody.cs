c35bc3425ccd9c2f3f5c9ac07bd588c87405ee94
msasaki-jyobi <mao.sasaki@morijyobi.ac.jp>
2024-11-29 01:45:50 +0900

    update

---
+++ Scripts/Attack/Body/SkinBody.cs
@@ -0,0 +1,13 @@
+using develop_common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace develop_body
+{
+    public class SkinBody : MonoBehaviour
+    {
+        public UnitComponents RootComponents;
+    }
+}
+
